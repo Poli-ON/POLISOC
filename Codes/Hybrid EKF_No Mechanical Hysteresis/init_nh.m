fc87f61230b5d55ae9c56f82648d0294701b7baf
Davide Clerici <davide.clerici@polito.it>
2025-09-10 18:05:40 +0200

    Update init_nh 1
    Improved the robustness of voltage and deformation inversion for initial soc estimation

--- Codes/Hybrid EKF_No Mechanical Hysteresis/init_nh.m
+++ Codes/Hybrid EKF_No Mechanical Hysteresis/init_nh.m
@@ -13,7 +13,7 @@
 % Output:
 %   Data: Data structure used by EKF algorithm code
  
-function Data = init(v0,d0,T0,SigmaX0,SigmaV,SigmaW,param,ObType)
+function Data = init_nh(v0,d0,T0,SigmaX0,SigmaV,SigmaW,param,ObType)
 
   % Initial state description: 
   % Save in the variable SOC0 the initial SOC computed by inverting the OCV
@@ -21,25 +21,27 @@ function Data = init(v0,d0,T0,SigmaX0,SigmaV,SigmaW,param,ObType)
   % obtained inverting the deformation in NMC and LCO cells (monotone THK
   % function).
 
+  cyc=param.cyc;
+
   switch ObType
       case 1
           % Voltage inversion
-          SOC0  = interp1(param.OCV,param.SOC,v0);                                  Data.zkInd = 1;
+          % SOC0  = interp1(param.OCV,param.SOC,min(max(v0,min(param.OCV)),max(param.OCV)));Data.zkInd = 1;
           % Deformation inversion
-          % SOC0  = interp1((param.DthkC{1}+flip(param.DthkD{1}))./2,param.SOC,d0); Data.zkInd = 1;
+          SOC0  = interp1((param.DthkC{cyc}+flip(param.DthkD{cyc}))./2,param.SOC,min(max(d0,min((param.DthkC{cyc}+flip(param.DthkD{cyc}))./2)),max((param.DthkC{cyc}+flip(param.DthkD{cyc}))./2))); Data.zkInd = 1; % If d0 is lower/greater than the min/max bound of the registered SOC-THK relationship, takes the boundary value. (Avoid NaN with iterp1).
           Data.xhat  = SOC0; % initial state
       case 2
-          ir0   = 0;                                                                Data.irInd = 1;
-          hk0   = 0;                                                                Data.hkInd = 2;
-          SOC0  = interp1(param.OCV,param.SOC,v0);                                  Data.zkInd = 3;
+          ir0   = 0;                                                                        Data.irInd = 1;
+          hk0   = 0;                                                                        Data.hekInd = 2;
+          SOC0  = interp1(param.OCV,param.SOC,min(max(v0,min(param.OCV)),max(param.OCV)));  Data.zkInd = 3; % If v0 is lower/greater than the min/max bound of the registered SOC-OCV relationship, takes the boundary value. (Avoid NaN with iterp1).
           Data.xhat  = [ir0 hk0 SOC0]'; % initial state   
       case 3
-          ir0   = 0;                                                                Data.irInd = 1;
-          hk0   = 0;                                                                Data.hkInd = 2;
+          ir0   = 0;                                                                        Data.irInd = 1;
+          hk0   = 0;                                                                        Data.hekInd = 2;
           % Voltage inversion
-          SOC0  = interp1(param.OCV,param.SOC,v0);                                  Data.zkInd = 3;
-          % Deformation inversion
-          % SOC0  = interp1((param.DthkC{1}+flip(param.DthkD{1}))./2,param.SOC,d0);   Data.zkInd = 3;
+          % SOC0  = interp1(param.OCV,param.SOC,min(max(v0,min(param.OCV)),max(param.OCV)));Data.zkInd = 3;
+          % Voltage + Deformation inversion
+          SOC0  = (interp1(param.OCV,param.SOC,min(max(v0,min(param.OCV)),max(param.OCV))) + interp1((param.DthkC{cyc}+flip(param.DthkD{cyc}))./2,param.SOC,min(max(d0,min((param.DthkC{cyc}+flip(param.DthkD{cyc}))./2)),max((param.DthkC{cyc}+flip(param.DthkD{cyc}))./2))))/2;   Data.zkInd = 3; % If v0 or d0 is lower/greater than the min/max bound of the registered SOC-OCV or SOC-THK relationship, takes the boundary value. (Avoid NaN with iterp1). 
           Data.xhat  = [ir0 hk0 SOC0]'; % initial state
   end
 
