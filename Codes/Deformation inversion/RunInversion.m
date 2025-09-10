35ee702e3cde50af3f0c2c44e83217f914b995e6
Davide Clerici <davide.clerici@polito.it>
2025-09-10 17:47:46 +0200

    Added a dataset. Improved robustness

--- Codes/Deformation inversion/RunInversion.m
+++ Codes/Deformation inversion/RunInversion.m
@@ -2,14 +2,14 @@ clear variables
 
 %% Load battery type parameter database
 
-BatType = 2;% 0 LCO, 1 LFP, 2 NMC
+BatType = 2;% 0 LCO, 1 LFP, 2 NMC, 3 NMC Michigan dataset
 
 switch BatType
     case 0 %LCO
         t0=9.2;             % Nominal battery thickness
         load param_LCO.mat
         param=param_LCO;
-        param.cyc=44;       % Set the aging level 1: first RPT, 44: last RPT. Reference: 10.5281/zenodo.14914172
+        param.cyc=1;       % Set the aging level 1: first RPT, 44: last RPT. Reference: 10.5281/zenodo.14914172
     case 1 %LFP
         t0=27;              % Nominal battery thickness
         load param_LFP10.mat
@@ -17,9 +17,16 @@ switch BatType
         param.cyc=1;
     case 2 %NMC
         t0=14;              % Nominal battery thickness
-        load param_NMC2.mat
-        param=param_NMC2;
+        load param_NMC1.mat
+        param=param_NMC1;
+        % param.DthkD{1}=param.DthkD{1}./1.05; % Attivare per prove a 40°C
+        % param.DthkC{1}=param.DthkC{1}./1.05; % Attivare per prove a 40°C
         param.cyc=1;
+    case 3 % NMC Michigan dataset
+        t0=4;                   % Nominal battery thickness
+        load param_NMC19.mat
+        param=param_NMC19;
+        param.cyc=6;            % 6 aging steps: 1: fresh, 2: 106 cycles, 3: 213 c. 4: 319 c. 5: 424 c.  6: 449c. (EOL)
 end
 
 
@@ -27,13 +34,16 @@ end
 
 switch BatType
     case 0 %LCO
-        load("Meas_LCO_DST1.mat")
+        load("Meas_LCO_DST2.mat")
     case 1 % LFP
         load("Meas_LFP10_DST1.mat")
         % load("Meas_LFP10_DriveCycle_1.mat")
     case 2 % NMC
-        % load("Meas_NMC2_DST1.mat")
-        load("Meas_NMC2_DriveCycle_3.mat")
+        % load("Meas_NMC1_DST1.mat")
+        load("Meas_NMC1_DriveCycle_1.mat")
+    case 3 % NMC Michigan dataset
+        load("Meas_NMC19.mat")
+        Meas=Meas{6};
 end
 
 % Compensate temperature on deformation measurements
@@ -61,18 +71,24 @@ switch BatType
         nominalCap                      = Ah(end); % Or, param.Q;
     case 2 %NMC
         nominalCap                      = Ah(end); % Or, param.Q;
+    case 3 %NMC Michigan dataset
+        nominalCap                      = 2*Ah(end); 
 end
-SOC0_cc                         = 1;
-soc_cc                          = (SOC0_cc * nominalCap - Ah)./nominalCap;  % Calculate the SOC using Coloumb Counting for comparison
-soc_current                     = (SOC0_cc * nominalCap - Ah_current_sensor)./nominalCap;
+SOC0_cc0                        = 1;
+soc_cc                          = (SOC0_cc0 * nominalCap - Ah)./nominalCap;                  % True soc computed with coulomb counting
+soc_current                     = (SOC0_cc0 * nominalCap - Ah_current_sensor)./nominalCap;   % Fictious SOC considering fictitious sensor errors
 T                               = 20;
 
 
 %% RUN
-
+tic
+Meas.Deformation(Meas.Deformation>max(param.DthkD{param.cyc}))=max(param.DthkD{param.cyc}); % Force deformation greater than the SOC-deformation characteristics to the max of the characteristic (SOC = 1)
+Meas.Deformation(Meas.Deformation<min(param.DthkD{param.cyc}))=min(param.DthkD{param.cyc}); % Force deformation lower than the SOC-deformation characteristics to the min of the characteristic (SOC = 0)
 soc=interp1((flip(param.DthkD{param.cyc}))/1,param.SOC,Meas.Deformation); % Invert the deformation characteristics recorded during discharge
-err=sqrt(mean((100*(soc(145:end)-soc_cc(145:end))).^2));
+% soc= soc - soc(1) + SOC0_cc0; % Force SOC0_cc0 to be initial soc.
+toc
 
+err=sqrt(mean((100*(soc-soc_cc)).^2));
 
 %% PLOT
 
